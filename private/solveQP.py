d86844cb37c0a1b4ec9f2cb4ea524ca315d6ad49
Buyun Liang <52502144+Buyun-Liang@users.noreply.github.com>
2021-09-09 12:43:20 -0500

    complete solve qp and qp termination

--- private/solveQP.py
+++ private/solveQP.py
@@ -1,9 +1,12 @@
-from dbg_print import dbg_print
+import torch
+from dbg_print import dbg_print, dbg_print_2
 # import gurobipy as gp
 # from gurobipy import GRB
 import osqp
 import numpy as np
 from scipy import sparse
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 QP_REQUESTS = 0
 
@@ -35,61 +38,17 @@ def solveQP(H,f,A,b,LB,UB,QPsolver):
     QP_REQUESTS += 1
 
     try:
-        # if QPsolver == "gurobi":
-            
-        #     # H,f always exist
-        #     # LB and UB always exist
-        #     #  formulation of QP has no 1/2
-        #     H = H/2
-
-        #     nvar = len(f)
-        #     # nvar = H.shape[0]
-        #     # Create a new model
-        #     m = gp.Model()
-        #     vtype = [GRB.CONTINUOUS] * nvar
-            
-        #     # Add variables to model
-        #     vars = []
-        #     for j in range(nvar):
-        #         vars.append(m.addVar(lb=LB[j], ub=UB[j], vtype=vtype[j]))
-        #     x_vec = np.array(vars).reshape(nvar,1)
-
-        #     if np.any(A != None) and np.any(b != None):
-        #         Aeq = A
-        #         beq = b
-        #         # Populate A matrix
-        #         expr = gp.LinExpr()
-        #         Ax = Aeq @ x_vec
-        #         expr += Ax[0,0]
-        #         m.addLConstr(expr, GRB.GREATER_EQUAL, beq)
-        #     else:
-        #         #  no constraint A*x < b
-        #         pass
-
-        #     solution = np.zeros((nvar,1))
-
-        #     # Populate objective: x.THx + f.T x
-        #     obj = gp.QuadExpr()
-        #     xTHx = x_vec.T @ H @ x_vec + f.T @ x_vec
-        #     obj += xTHx[0,0]
-        #     m.setObjective(obj)
-
-        #     #  suppress output
-        #     # m.Params.LogToConsole = 0
-        #     m.Params.outputflag = 0
-        #     # m.params.NonConvex = 2
-
-        #     m.optimize()
-        #     x = m.getAttr('x', vars)
-        #     for i in range(nvar):
-        #         solution[i,0] = x[i]
-
-        #     return solution
-
         if QPsolver == "osqp":
             # H,f always exist
             nvar = len(f)
             # H and A has to be sparse
+            dbg_print_2("NOTE: inefficient convert torch tensor to numpy to sparse")
+            H = H.cpu().numpy()
+            f = f.cpu().numpy()
+            A = A.cpu().numpy()
+            # b = b.cpu().numpy()
+            LB = LB.cpu().numpy()
+            UB = UB.cpu().numpy()
             H_sparse = sparse.csc_matrix(H)
             # LB and UB always exist
 
@@ -118,6 +77,7 @@ def solveQP(H,f,A,b,LB,UB,QPsolver):
             solution = res.x
             sol_len = solution.size
             solution = solution.reshape((sol_len,1))
+            solution = torch.from_numpy(solution).to(device=device, dtype=torch.double) 
             return solution
 
     except Exception as e:
