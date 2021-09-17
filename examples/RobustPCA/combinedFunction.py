f2ddd0742d9c4e049244e9328d7d376cb67c5d39
Buyun Liang <52502144+Buyun-Liang@users.noreply.github.com>
2021-09-17 16:24:07 -0500

    clean code for robust PCA problem

--- examples/RobustPCA/combinedFunction.py
+++ examples/RobustPCA/combinedFunction.py
@@ -8,11 +8,9 @@ def eval_obj(X_struct,data_in = None):
     S = X_struct.S
     M.requires_grad_(True)
     S.requires_grad_(True)
-    
     # obtain data_in from runExample.py
     eta = data_in.eta
-    Y = data_in.Y
-    
+
     # objective function
     f = torch.norm(M, p = 'nuc') + eta * torch.norm(S, p = 1)
     return f
@@ -26,16 +24,15 @@ def combinedFunction(X_struct,data_in = None):
     S.requires_grad_(True)
     
     # # obtain data_in from runExample.py
-    # eta = data_in.eta
+    eta = data_in.eta
     Y = data_in.Y
     
     # objective function
-    # f = torch.norm(M, p = 'nuc') + eta * torch.norm(S, p = 1)
-    f = eval_obj(X_struct,data_in)
+    f = torch.norm(M, p = 'nuc') + eta * torch.norm(S, p = 1)
 
     # inequality constraint, matrix form
     ci = None
-
+    
     # equality constraint 
     ce = general_struct()
     ce.c1 = M + S - Y
