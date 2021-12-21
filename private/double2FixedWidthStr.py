2b00d08cf3132df34d5a5a3000e103d1d23da76d
Buyun-Liang <liang664@umn.edu>
2021-12-20 20:04:27 -0600

    double2FixedWidthStr docs

--- private/double2FixedWidthStr.py
+++ private/double2FixedWidthStr.py
@@ -4,13 +4,132 @@ from private import nDigitsInWholePart as nDIWP
 def double2FixedWidthStr(width, str_in = None):
     """
     double2FixedWidthStr:
-      Formats number x in the desired width field so that the length of
-      the string remains constant for all values of x and the most
-      significant digit is always in the second place (so sign changes do
-      not shift alignment and a '+' is not necessary).  The precision
-      displayed is always at least 2 digits and varies on the magnitude
-      of the number, as numbers requiring scientific notation require
-      more characters to display their magnitude.
+        Formats number x in the desired width field so that the length of
+        the string remains constant for all values of x and the most
+        significant digit is always in the second place (so sign changes do
+        not shift alignment and a '+' is not necessary).  The precision
+        displayed is always at least 2 digits and varies on the magnitude
+        of the number, as numbers requiring scientific notation require
+        more characters to display their magnitude.
+
+        USAGE:
+            1) Get a function handle to do formatting for a given width
+            format_fn   = double2FixedWidth(width);
+        
+            2) Format a number to a string of a given width
+            str         = double2FixedWidth(number,width);
+        
+        INPUT:
+            width       An integer between 9 and 23 specifying the number
+                        of chars to use for representing a number as a string.
+                        For width == 9, the string representation will always
+                        show the first two most significant digits while width
+                        == 23, the representation will show all the significant
+                        digits up to machine precision.
+        
+            number      A double to convert to a fixed width string.
+        
+        OUTPUT:         [Either one of the following] 
+            format_fn   Function handle that takes single argument x and
+                        converts it to a string representation with fixed width
+                        such that the most significant digit will always be the
+                        second character.  Note that if x is a number, variable
+                        precision is used to ensure the width of the string
+                        representation does not change.  Nonnumeric x will
+                        cause a centered 'n/a' to be returned.  Both infs 
+                        (positive and negative) and NaNs are supported.
+        
+            str         fixed width aligned string representation of number. 
+
+        If you publish work that uses or refers to NCVX, please cite both
+        NCVX and GRANSO paper:
+
+        [1] Buyun Liang, and Ju Sun. 
+            NCVX: A User-Friendly and Scalable Package for Nonconvex 
+            Optimization in Machine Learning. arXiv preprint arXiv:2111.13984 (2021).
+            Available at https://arxiv.org/abs/2111.13984
+
+        [2] Frank E. Curtis, Tim Mitchell, and Michael L. Overton 
+            A BFGS-SQP method for nonsmooth, nonconvex, constrained 
+            optimization and its evaluation using relative minimization 
+            profiles, Optimization Methods and Software, 32(1):148-181, 2017.
+            Available at https://dx.doi.org/10.1080/10556788.2016.1208749
+            
+        Change Log:
+            double2FixedWidthStr.m introduced in GRANSO Version 1.0
+            
+            Buyun Dec 20, 2021 (NCVX Version 1.0.0):
+                double2FixedWidthStr.py is translated from double2FixedWidthStr.m in GRANSO Version 1.6.4. 
+
+        For comments/bug reports, please visit the NCVX webpage:
+        https://github.com/sun-umn/NCVX
+        
+        NCVX Version 1.0.0, 2021, see AGPL license info below.
+
+        =========================================================================
+        |  double2FixedWidthStr.m                                               |
+        |  Copyright (C) 2016 Tim Mitchell                                      |
+        |                                                                       |
+        |  This file is originally from URTM.                                   |
+        |                                                                       |
+        |  URTM is free software: you can redistribute it and/or modify         |
+        |  it under the terms of the GNU Affero General Public License as       |
+        |  published by the Free Software Foundation, either version 3 of       |
+        |  the License, or (at your option) any later version.                  |
+        |                                                                       |
+        |  URTM is distributed in the hope that it will be useful,              |
+        |  but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+        |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+        |  GNU Affero General Public License for more details.                  |
+        |                                                                       |
+        |  You should have received a copy of the GNU Affero General Public     |
+        |  License along with this program.  If not, see                        |
+        |  <http://www.gnu.org/licenses/agpl.html>.                             |
+        =========================================================================
+
+        =========================================================================
+        |  GRANSO: GRadient-based Algorithm for Non-Smooth Optimization         |
+        |  Copyright (C) 2016 Tim Mitchell                                      |
+        |                                                                       |
+        |  This file is translated from GRANSO.                                 |
+        |                                                                       |
+        |  GRANSO is free software: you can redistribute it and/or modify       |
+        |  it under the terms of the GNU Affero General Public License as       |
+        |  published by the Free Software Foundation, either version 3 of       |
+        |  the License, or (at your option) any later version.                  |
+        |                                                                       |
+        |  GRANSO is distributed in the hope that it will be useful,            |
+        |  but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+        |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+        |  GNU Affero General Public License for more details.                  |
+        |                                                                       |
+        |  You should have received a copy of the GNU Affero General Public     |
+        |  License along with this program.  If not, see                        |
+        |  <http://www.gnu.org/licenses/agpl.html>.                             |
+        =========================================================================
+
+        =========================================================================
+        |  NCVX (NonConVeX): A User-Friendly and Scalable Package for           |
+        |  Nonconvex Optimization in Machine Learning.                          |
+        |                                                                       |
+        |  Copyright (C) 2021 Buyun Liang                                       |
+        |                                                                       |
+        |  This file is part of NCVX.                                           |
+        |                                                                       |
+        |  NCVX is free software: you can redistribute it and/or modify         |
+        |  it under the terms of the GNU Affero General Public License as       |
+        |  published by the Free Software Foundation, either version 3 of       |
+        |  the License, or (at your option) any later version.                  |
+        |                                                                       |
+        |  GRANSO is distributed in the hope that it will be useful,            |
+        |  but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+        |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+        |  GNU Affero General Public License for more details.                  |
+        |                                                                       |
+        |  You should have received a copy of the GNU Affero General Public     |
+        |  License along with this program.  If not, see                        |
+        |  <http://www.gnu.org/licenses/agpl.html>.                             |
+        =========================================================================
     """
     #  need places for
     #  - sign (1 char)
