08faae2411024e25f22a393a45089af97511a47c
Buyun-Liang <liang664@umn.edu>
2021-12-20 19:28:01 -0600

    docs for centerstring

--- private/centerString.py
+++ private/centerString.py
@@ -7,6 +7,73 @@ def centerString(s,width):
        side.  If s is longer than width, s will be truncated. If an odd
        number of spaces is needed, the left side will have one more space
        compared to the right side.
+
+        If you publish work that uses or refers to NCVX, please cite both
+        NCVX and GRANSO paper:
+
+        [1] Buyun Liang, and Ju Sun. 
+            NCVX: A User-Friendly and Scalable Package for Nonconvex 
+            Optimization in Machine Learning. arXiv preprint arXiv:2111.13984 (2021).
+            Available at https://arxiv.org/abs/2111.13984
+
+        [2] Frank E. Curtis, Tim Mitchell, and Michael L. Overton 
+            A BFGS-SQP method for nonsmooth, nonconvex, constrained 
+            optimization and its evaluation using relative minimization 
+            profiles, Optimization Methods and Software, 32(1):148-181, 2017.
+            Available at https://dx.doi.org/10.1080/10556788.2016.1208749
+            
+        Change Log:
+            centerString.m introduced in GRANSO Version 1.0
+            
+            Buyun Dec 20, 2021 (NCVX Version 1.0.0):
+                centerString.py is translated from centerString.m in GRANSO Version 1.6.4. 
+
+        For comments/bug reports, please visit the NCVX webpage:
+        https://github.com/sun-umn/NCVX
+        
+        GRANSO Version 1.6.4, 2016-2020, see AGPL license info below.
+
+        =========================================================================
+        |  centerString.m                                                       |
+        |  Copyright (C) 2016 Tim Mitchell                                      |
+        |                                                                       |
+        |  This file is originally from URTM.                                   |
+        |                                                                       |
+        |  URTM is free software: you can redistribute it and/or modify         |
+        |  it under the terms of the GNU Affero General Public License as       |
+        |  published by the Free Software Foundation, either version 3 of       |
+        |  the License, or (at your option) any later version.                  |
+        |                                                                       |
+        |  URTM is distributed in the hope that it will be useful,              |
+        |  but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+        |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+        |  GNU Affero General Public License for more details.                  |
+        |                                                                       |
+        |  You should have received a copy of the GNU Affero General Public     |
+        |  License along with this program.  If not, see                        |
+        |  <http://www.gnu.org/licenses/agpl.html>.                             |
+        =========================================================================
+
+        =========================================================================
+        |  GRANSO: GRadient-based Algorithm for Non-Smooth Optimization         |
+        |  Copyright (C) 2016 Tim Mitchell                                      |
+        |                                                                       |
+        |  This file is part of GRANSO.                                         |
+        |                                                                       |
+        |  GRANSO is free software: you can redistribute it and/or modify       |
+        |  it under the terms of the GNU Affero General Public License as       |
+        |  published by the Free Software Foundation, either version 3 of       |
+        |  the License, or (at your option) any later version.                  |
+        |                                                                       |
+        |  GRANSO is distributed in the hope that it will be useful,            |
+        |  but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+        |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+        |  GNU Affero General Public License for more details.                  |
+        |                                                                       |
+        |  You should have received a copy of the GNU Affero General Public     |
+        |  License along with this program.  If not, see                        |
+        |  <http://www.gnu.org/licenses/agpl.html>.                             |
+        =========================================================================
     """
 
     s                   = s.strip()
