5afe87096a01944245e622abc9a86caf48fbb7e1
Buyun-Liang <liang664@umn.edu>
2022-01-17 20:18:53 -0600

    update installation guidance for mac and linux

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pygranso",
-    version="1.0.0-dev1",
+    version="1.0.0-dev2",
     author="Tim Mitchell and Buyun Liang",
     author_email="liang664@umn.edu, tim@timmitchell.com",
     description="PyGRANSO: A PyTorch-enabled port of GRANSO with auto-differentiation",
@@ -19,13 +19,4 @@ setuptools.setup(
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
     python_requires=">=3.9",
-    install_requires=[
-        "torch==1.9.0+cu111",
-        "torchvision==0.10.0+cu111",
-        "torchaudio==0.9.0",
-        "osqp >= 0.6.2",
-        "numpy >= 1.20.3",
-        "scipy >= 1.7.1",
-        "notebook >= 6.4.5"
-    ],
 )
