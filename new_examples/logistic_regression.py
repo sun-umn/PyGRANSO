d57d5437c92b317e0e7dce8dca51f4440fca47bf
Buyun-Liang <liang664@umn.edu>
2022-03-06 21:01:45 -0600

    update log reg: to be add to ncvx.org

--- new_examples/logistic_regression.py
+++ new_examples/logistic_regression.py
@@ -26,13 +26,14 @@ X /= X.max()  # Normalize X to speed-up convergence
 
 # Demo path functions
 
-# cs = l1_min_c(X, y, loss="log") * np.logspace(7, 9, 2)
-cs = l1_min_c(X, y, loss="log") * np.logspace(0, 16, 16)
+cs = l1_min_c(X, y, loss="log") * np.logspace(0, 7, 16)
+# cs = l1_min_c(X, y, loss="log") * np.logspace(0, 5, 4)
 
 
 X = torch.from_numpy(X).to(device=device, dtype=torch.double)
 y = torch.from_numpy(y).to(device=device, dtype=torch.double)
 n = y.shape[0]
+y = y.unsqueeze(1)
 # #############################################################################
 
 
@@ -47,11 +48,15 @@ def user_fn(X_struct,X,y,C):
     w = X_struct.w
 
     # objective function
-    for i in range(n):
-        if i == 0:
-            f = torch.log(torch.exp(-y[i]* X[i,:]@w) + 1)
-        else:
-            f += torch.log(torch.exp(-y[i]* X[i,:]@w) + 1)
+    # for i in range(n):
+    #     if i == 0:
+    #         f = torch.log(torch.exp(-y[i]* X[i,:]@w) + 1)
+    #     else:
+    #         f += torch.log(torch.exp(-y[i]* X[i,:]@w) + 1)
+
+    # torch.reshape(y,(n,1))
+    
+    f = torch.sum(torch.log(torch.exp(-y* (X@w)) + 1))
     f+= torch.norm(w,p=1)/C
 
     # inequality constraint, matrix form
@@ -78,15 +83,19 @@ start = time()
 
 opts = pygransoStruct()
 opts.torch_device = device
-opts.maxit = 100
-opts.opt_tol = 1e-3
+opts.maxit = 50
+opts.opt_tol = 1e-6
 np.random.seed(1)
 opts.x0 = torch.zeros(4,1).to(device=device, dtype=torch.double)
+# torch.manual_seed(0)
+# opts.x0 = torch.randn(4, 1).to(device=device, dtype=torch.double)
 # opts.print_frequency = 10
 
 coefs_ = []
+i = 0
 for c in cs:
-    print("c = {}".format(c))
+    print("c = {}, i = {}".format(c,i))
+    i += 1
     comb_fn = lambda X_struct : user_fn(X_struct,X,y,c)
     torch.autograd.set_detect_anomaly(True)
     soln = pygranso(var_spec = var_in,combined_fn = comb_fn,user_opts = opts)
