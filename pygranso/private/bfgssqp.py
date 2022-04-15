fix stationarity QP numerical issue (nonconvex qp)

pygranso/private/bfgssqp.py
@@ -8,7 +8,7 @@ from pygranso.pygransoStruct import pygransoStruct
 import math
 import numpy as np
 from numpy.random import default_rng
-import traceback,sys
+import traceback
 
 class AlgBFGSSQP():
     def __init__(self):
@@ -245,7 +245,7 @@ class AlgBFGSSQP():
         self.get_nearby_grads_fn     = lambda : getNearbyGradients( self.penaltyfn_obj, get_nbd_grads_fn)
 
         [stat_vec,self.stat_val,qps_solved, _, _] = self.computeApproxStationarityVector()
-    
+
         if not self.constrained:
             #  disable steering QP solves by increasing min_fallback_level.
             min_fallback_level  = max(  min_fallback_level, POSTQP_FALLBACK_LEVEL )
@@ -331,7 +331,7 @@ class AlgBFGSSQP():
                 except Exception as e:
                     print("PyGRANSO:steeringQuadprogFailure")
                     print(traceback.format_exc())
-                    self.prepareTermination(12); # qp failure
+                    self.prepareTermination(12) # qp failure
                     return self.info
                     # sys.exit()
                 
@@ -342,7 +342,7 @@ class AlgBFGSSQP():
             elif self.fallback_level == 2:
                 p = -self.apply_H_fn(g)   # standard BFGS 
             elif self.fallback_level == 3:
-                p = -g;     # steepest descent
+                p = -g     # steepest descent
 
                 
             else:
@@ -360,7 +360,7 @@ class AlgBFGSSQP():
                 if self.bumpFallbackLevel():
                     continue    # try iteration again with new fallback
                 else: # all fallbacks have failed - quit!
-                    self.prepareTermination(9); # not a descent descent direction
+                    self.prepareTermination(9) # not a descent descent direction
                     return self.info
                 
             else: # ATTEMPT LINE SEARCH
@@ -425,7 +425,7 @@ class AlgBFGSSQP():
 
             # for stationarity condition
             [ stat_vec, self.stat_val, qps_solved, n_grad_samples,_]   = self.computeApproxStationarityVector()
-                
+
             
             ls_evals = self.penaltyfn_obj.getNumberOfEvaluations()-evals_so_far
             
@@ -577,7 +577,7 @@ class AlgBFGSSQP():
         stat_vec        = self.penaltyfn_at_x.p_grad
         stat_value      = torch.norm(stat_vec)
 
-        
+
         self.opt_tol = torch.as_tensor(self.opt_tol,device = self.torch_device, dtype=self.torch_dtype)
 
         if stat_value <= self.opt_tol:
@@ -608,8 +608,7 @@ class AlgBFGSSQP():
         except Exception as e:
             print("PyGRANSO:terminationQuadprogFailure")
             print(traceback.format_exc())
-            self.prepareTermination(12); # not a descent descent direction
-            return self.info
+            [stat_vec,n_qps,ME] = [ None, 1, None] # set a very large stat vec
 
         stat_value = torch.norm(stat_vec).item()
         self.penaltyfn_obj.addStationarityMeasure(stat_value)

pygranso/private/qpTerminationCondition.py
@@ -1,9 +1,8 @@
 import numpy as np
 import torch
 from pygranso.private.solveQP import solveQP
-from numpy import conjugate as conj
 from numpy import linalg as LA
-import traceback,sys
+import traceback
 
 class qpTC:
     def __init__(self):
@@ -76,6 +75,9 @@ class qpTC:
 
             This is a direct port of qpTerminationCondition.m from GRANSO v1.6.4.
             Ported from MATLAB to Python by Buyun Liang 2021.
+
+            v1.2.0: fix stationarity measure problem. recalculate stationarity when 
+            encounter (nonconvex) numerical issue in QP
 
             For comments/bug reports, please visit the PyGRANSO webpage:
             https://github.com/sun-umn/PyGRANSO
@@ -158,19 +160,20 @@ class qpTC:
             #  formulation of QP has no 1/2
             self.solveQP_fn = lambda H: solveQP(H,f,Aeq,beq,LB,UB,QPsolver,torch_device, double_precision)
 
-        [y,_,qps_solved,ME] = self.solveQPRobust()
+        [y,_,qps_solved,ME] = self.solveQPRobust(torch_dtype)
       
         #  If the QP solve(s) failed, return infinite vector so it can't
         #  possibly trigger BFGS-SQP's convergence criteria
-        if y.size == 0:
+        if y == None:
             #  its length is equal to the number of variables
-            d = np.inf * np.ones((Hinv_grads.shape[0],1)); 
+            d = np.inf * np.ones((Hinv_grads.shape[0],1))
+            d = torch.from_numpy(d).to(device=torch_device, dtype=torch_dtype)
         else:
             d = -Hinv_grads @ y
 
         return [d,qps_solved,ME]
 
-    def solveQPRobust(self):
+    def solveQPRobust(self,torch_dtype):
        
         x       = None
         lambdas = None # not used here
@@ -193,8 +196,8 @@ class qpTC:
         #  Fall back strategy #1: replace Hinv with identity and try again
         try:
             stat_type = 2
-            R = conj(self.all_grads.T) @ self.all_grads
-            R = (R + conj(R.T))/2
+            R = torch.conj(self.all_grads.T) @ self.all_grads
+            R = (R + torch.conj(R.T))/2
             x = self.solveQP_fn(R)
             return [x,lambdas,stat_type,ME]
         except Exception as e:
@@ -212,9 +215,15 @@ class qpTC:
 
         try:
             stat_type = 4
-            [D,V] = LA.eig(self.H)
-            dvec = [x if x >= 0 else 0 for x in D]
-            Hreg = conj(V.T) @ np.diag(dvec) @ conj(V.T)
+            [D,V] = torch.linalg.eig(self.H)
+            D = D.real
+            V = V.real
+            D.to(dtype=torch_dtype)
+            
+            dvec = D.clone()
+            dvec[D<0] = 0
+            # dvec = [x if x >= 0 else 0 for x in D]
+            Hreg = torch.conj(V.T) @ torch.diag(dvec) @ torch.conj(V.T)
             x = self.solveQP_fn(Hreg)
             return [x,lambdas,stat_type,ME]
         except Exception as e:

pygranso/private/solveQP.py
@@ -90,121 +90,114 @@ def solveQP(H,f,A,b,LB,UB,QPsolver,torch_device, double_precision):
     global QP_REQUESTS
     QP_REQUESTS += 1
 
-    try:
-        if QPsolver == "osqp":
-            # H,f always exist
-            nvar = len(f)
-            # H and A has to be sparse
-            H = H.cpu().numpy()
-            f = f.cpu().numpy()
-            if A != None:
-                A = A.cpu().numpy()
-            # b = b.cpu().numpy()
-            LB = LB.cpu().numpy()
-            UB = UB.cpu().numpy()
-            H_sparse = sparse.csc_matrix(H)
-            # LB and UB always exist
+    if QPsolver == "osqp":
+        # H,f always exist
+        nvar = len(f)
+        # H and A has to be sparse
+        H = H.cpu().numpy()
+        f = f.cpu().numpy()
+        if A != None:
+            A = A.cpu().numpy()
+        # b = b.cpu().numpy()
+        LB = LB.cpu().numpy()
+        UB = UB.cpu().numpy()
+        H_sparse = sparse.csc_matrix(H)
+        # LB and UB always exist
 
-            if np.any(A != None) and np.any(b != None):
-                Aeq = A
-                beq = b
-                speye = sparse.eye(nvar)
-                LB_new = np.vstack((beq,LB))
-                UB_new = np.vstack((beq,UB))
-                A_new = sparse.vstack([Aeq,speye])
-                A_new = sparse.csc_matrix(A_new)
-            else:
-                #  no constraint A*x == b
-                A_new = sparse.eye(nvar)
-                A_new = sparse.csc_matrix(A_new)
-                LB_new = LB
-                UB_new = UB
+        if np.any(A != None) and np.any(b != None):
+            Aeq = A
+            beq = b
+            speye = sparse.eye(nvar)
+            LB_new = np.vstack((beq,LB))
+            UB_new = np.vstack((beq,UB))
+            A_new = sparse.vstack([Aeq,speye])
+            A_new = sparse.csc_matrix(A_new)
+        else:
+            #  no constraint A*x == b
+            A_new = sparse.eye(nvar)
+            A_new = sparse.csc_matrix(A_new)
+            LB_new = LB
+            UB_new = UB
 
-            # Create an OSQP object
-            prob = osqp.OSQP()
+        # Create an OSQP object
+        prob = osqp.OSQP()
 
-            # Setup workspace and change alpha parameter
-            prob.setup(H_sparse, f, A_new, LB_new, UB_new, eps_abs = 1e-12, eps_rel=1e-12, polish=True, verbose=False)
-            # prob.setup(H_sparse, f, A_new, LB_new, UB_new, alpha=1.0,verbose=False)
+        # Setup workspace and change alpha parameter
+        prob.setup(H_sparse, f, A_new, LB_new, UB_new, eps_abs = 1e-12, eps_rel=1e-12, polish=True, verbose=False)
+        # prob.setup(H_sparse, f, A_new, LB_new, UB_new, alpha=1.0,verbose=False)
 
-            # Solve problem
-            res = prob.solve()
+        # Solve problem
+        res = prob.solve()
 
-            solution = res.x
-            sol_len = solution.size
-            solution = solution.reshape((sol_len,1))
-            if double_precision:
-                torch_dtype = torch.double
-            else:
-                torch_dtype = torch.float
-            solution = torch.from_numpy(solution).to(device=torch_device, dtype=torch_dtype) 
-            return solution
+        solution = res.x
+        sol_len = solution.size
+        solution = solution.reshape((sol_len,1))
+        if double_precision:
+            torch_dtype = torch.double
+        else:
+            torch_dtype = torch.float
+        solution = torch.from_numpy(solution).to(device=torch_device, dtype=torch_dtype) 
+        return solution
 
-        if QPsolver == "gurobi":
+    if QPsolver == "gurobi":
 
-            H = H.cpu().numpy()
-            f = f.cpu().numpy()
-            if A != None:
-                A = A.cpu().numpy()
-            # H,f always exist
-            # LB and UB always exist
-            #  formulation of QP has no 1/2
-            H = H/2
+        H = H.cpu().numpy()
+        f = f.cpu().numpy()
+        if A != None:
+            A = A.cpu().numpy()
+        # H,f always exist
+        # LB and UB always exist
+        #  formulation of QP has no 1/2
+        H = H/2
 
-            nvar = len(f)
-            # Create a new model
-            m = gp.Model()
-            vtype = [GRB.CONTINUOUS] * nvar
+        nvar = len(f)
+        # Create a new model
+        m = gp.Model()
+        vtype = [GRB.CONTINUOUS] * nvar
 
-            # Add variables to model
-            vars = []
-            for j in range(nvar):
-                vars.append(m.addVar(lb=LB[j], ub=UB[j], vtype=vtype[j]))
-            x_vec = np.array(vars).reshape(nvar,1)
+        # Add variables to model
+        vars = []
+        for j in range(nvar):
+            vars.append(m.addVar(lb=LB[j], ub=UB[j], vtype=vtype[j]))
+        x_vec = np.array(vars).reshape(nvar,1)
 
-            if np.any(A != None) and np.any(b != None):
-                Aeq = A
-                beq = b
-                # Populate A matrix
-                expr = gp.LinExpr()
-                Ax = Aeq @ x_vec
-                expr += Ax[0,0]
-                m.addLConstr(expr, GRB.GREATER_EQUAL, beq)
-            else:
-                #  no constraint A*x < b
-                pass
+        if np.any(A != None) and np.any(b != None):
+            Aeq = A
+            beq = b
+            # Populate A matrix
+            expr = gp.LinExpr()
+            Ax = Aeq @ x_vec
+            expr += Ax[0,0]
+            m.addLConstr(expr, GRB.GREATER_EQUAL, beq)
+        else:
+            #  no constraint A*x < b
+            pass
 
-            solution = np.zeros((nvar,1))
+        solution = np.zeros((nvar,1))
 
-            # Populate objective: x.THx + f.T x
-            obj = gp.QuadExpr()
-            xTHx = x_vec.T @ H @ x_vec + f.T @ x_vec
-            obj += xTHx[0,0]
-            m.setObjective(obj)
+        # Populate objective: x.THx + f.T x
+        obj = gp.QuadExpr()
+        xTHx = x_vec.T @ H @ x_vec + f.T @ x_vec
+        obj += xTHx[0,0]
+        m.setObjective(obj)
 
-            #  suppress output
-            # m.Params.LogToConsole = 0
-            m.Params.outputflag = 0
-            # m.params.NonConvex = 2
+        #  suppress output
+        # m.Params.LogToConsole = 0
+        m.Params.outputflag = 0
+        # m.params.NonConvex = 2
 
-            m.optimize()
-            x = m.getAttr('x', vars)
-            for i in range(nvar):
-                solution[i] = x[i]
+        m.optimize()
+        x = m.getAttr('x', vars)
+        for i in range(nvar):
+            solution[i] = x[i]
 
-            if double_precision:
-                torch_dtype = torch.double
-            else:
-                torch_dtype = torch.float
-            solution = torch.from_numpy(solution).to(device=torch_device, dtype=torch_dtype) 
-            return solution
+        if double_precision:
+            torch_dtype = torch.double
+        else:
+            torch_dtype = torch.float
+        solution = torch.from_numpy(solution).to(device=torch_device, dtype=torch_dtype) 
+        return solution
 
-    except Exception as e:
-        [w,v] = np.linalg.eigh(H)
-        w_sorted = np.sort(w)
-        print(w_sorted)
-        print(traceback.format_exc())
-        # sys.exit()
 
 
 def getErr():